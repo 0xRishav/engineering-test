66b09528f663d0a162196d23fba9981216ff0737
Rishav Bharti <64918176+iamrishavb@users.noreply.github.com>
2021-10-13 01:29:10 +0530

    refactor: make different file for types

--- front-end/src/contexts/student-context.tsx
+++ front-end/src/contexts/student-context.tsx
@@ -5,30 +5,7 @@ import { SortDirection, SortBy } from "shared/models/sort"
 import { RollStateFilterType } from "shared/models/roll"
 import { Student } from "shared/models/person"
 import { Reducer } from "react"
-
-export type Action =
-  | { type: "SET_SORTED_STUDENTS"; payload: { allStudents: Student[] } }
-  | { type: "SET_FILTERED_STUDENTS" }
-  | { type: "SET_ROLL_STATE_FILTER"; payload: { type: RollStateFilterType | null } }
-  | { type: "SET_SEARCH_INPUT"; payload: { searchInput: string } }
-  | { type: "CHANGE_SORT_BY" }
-  | { type: "CHANGE_SORT_DIRECTION" }
-  | { type: "CREATE_ROLL_SNAPSHOT"; payload: { snapshot: Student[] } }
-
-interface InitialState {
-  sortDirection: SortDirection
-  searchInput: string
-  sortBy: SortBy
-  sortedAndFilteredStudents: Student[]
-  rollStateFilter: RollStateFilterType | null
-  sortedStudents: Student[]
-  rollSnapShot: Student[]
-}
-
-type StudentContextType = {
-  state: InitialState
-  dispatch: Dispatch<Action>
-}
+import { Action, InitialState, StudentContextType } from "./student-context.types"
 
 const initialState: InitialState = {
   sortDirection: "ASC",
